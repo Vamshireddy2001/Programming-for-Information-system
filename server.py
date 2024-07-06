9e10648657f4b02ee8c8b99042759a54d168429b
Vamshireddy2001 <Vamshireddymaram101@gmail.com>
2024-07-07 00:05:35 +0530

    routesadded

--- server.py
+++ server.py
@@ -7,8 +7,11 @@ app=Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template('mansection.html')
 
+@app.route("/women")
+def womensection():
+    return render_template('womensection.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
