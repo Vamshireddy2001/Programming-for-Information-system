3c45f8f5df25c103a9244c0e34851bb912f8460c
Sravs9161 <20037741@mydbs.ie>
2024-07-12 02:02:46 +0530

    database installed

--- server.py
+++ server.py
@@ -1,6 +1,8 @@
 from flask import Flask,render_template
 app=Flask(__name__)
+from pymongo import MongoClient
 
+client = MongoClient('mongodb+srv://20037741:sravas1234@cluster0.lvqco1z.mongodb.net/')
 
 
 @app.route("/collections")
