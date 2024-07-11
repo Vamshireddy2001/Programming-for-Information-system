a8665e5d9f56f2cad3e5aba1ea21b5274e85b04d
Sravs9161 <20037741@mydbs.ie>
2024-07-12 02:17:42 +0530

    addmongodb

--- server.py
+++ server.py
@@ -1,9 +1,22 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,redirect,request,url_for
 app=Flask(__name__)
 from pymongo import MongoClient
 
 client = MongoClient('mongodb+srv://20037741:sravas1234@cluster0.lvqco1z.mongodb.net/')
 
+db = client['FabricLove']
+
+class User:
+    def __init__(self, name, email,password):
+        self.name = name
+        self.email = email
+        self.password=password
+
+    def save(self):
+        user_dict = self.__dict__
+        result = db.users.insert_one(user_dict)
+        return result.inserted_id
+    
 
 @app.route("/collections")
 def collectionsRoute():
@@ -25,6 +38,18 @@ def login():
 
 @app.route("/register")
 def register():
+    if request.method == 'POST':
+        # Get form data
+        name = request.form.get('name')
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        # Save user to MongoDB
+        result = db.users.insert_one({'name': name, 'email': email,password:password})
+        print(f"Inserted document ID: {result.inserted_id}")
+
+        return render_template('registerpage.html')
+
     return render_template('registerpage.html')
 
 
