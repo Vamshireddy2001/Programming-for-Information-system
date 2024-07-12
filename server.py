c2700ce21cb06d50427a1edc28d5460c271c2bad
Sravs9161 <20037741@mydbs.ie>
2024-07-12 15:54:41 +0530

    errors

--- server.py
+++ server.py
@@ -1,11 +1,11 @@
-from flask import Flask,render_template,redirect,request,url_for
+from flask import Flask,render_template,redirect,request,url_for,flash,jsonify
 app=Flask(__name__)
 from pymongo import MongoClient
 
 client = MongoClient('mongodb+srv://20037741:sravas1234@cluster0.lvqco1z.mongodb.net/')
 
 db = client['FabricLove']
-
+app.secret_key = '12345'
 class User:
     def __init__(self, name, email,password):
         self.name = name
@@ -39,20 +39,32 @@ def login():
 @app.route("/register",methods=['GET','POST'])
 def register():
     if request.method == 'POST':
-        print('works')
         # Get form data
         name = request.form.get('name')
         email = request.form.get('email')
         password = request.form.get('password')
-        
-        print(name,email,password)
+        confirmPassword = request.form.get('confirmpassword')
+
+        message=CheckForNames(name,email,password,confirmPassword)
         # result = db.users.insert_one({'name': name, 'email': email,password:password})
         # print(f"Inserted document ID: {result.inserted_id}")
+         
+        return render_template('registerpage.html',message=message)
 
-        return render_template('registerpage.html')
+    return render_template('registerpage.html',message="")
 
-    return render_template('registerpage.html')
-
+def CheckForNames(name,email,password,confirmpassword):
+    if(name==""):
+        return jsonify({'error': 'Method not allowed'}), 405
+    elif(email==""):
+        return ('email not be blank')
+        
+    elif(password==""):
+        return('password not be blank')
+        
+    elif(confirmpassword!=password):
+        return('password not match!')
+        
 
 if __name__ == "__main__":
     app.run(debug=True)
